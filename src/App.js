37fd4825f290a25def9c3826c219e64f677f54e8
Elliot <Elliot.palmstadius@gmail.com>
2021-11-04 10:35:53 +0100

    PC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Box } from 'theme-ui'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Box sx={{ p: 3 }} className="App-Header">
+          <div >
+            <h1>Header</h1>
+          </div>
+        </Box>
+        <Box sx={{ flex: '1 1 auto', p: 3 }} className="App-Content">
+          <div >
+            <h2>Content</h2>
+          </div>
+        </Box>
+        <Box sx={{ p: 3 }} className="App-Footer">
+          <div >
+            <h3>Footer</h3>
+          </div>
+        </Box>
+      </Box>
     </div>
   );
 }
